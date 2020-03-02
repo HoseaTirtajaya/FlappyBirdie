9e507b44f8ee67f44c4cd3aba1edd46321593ac1
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-03-03 01:14:52 +0700

    Initial Commit :star2:

--- Source Code/Assets/Scripts/Pipe.cs
+++ Source Code/Assets/Scripts/Pipe.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour
 {
+    //Global variable
+    [SerializeField] private Bird bird;
+    [SerializeField] private float speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,32 @@ public class Pipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!bird.IsDead())
+        {
+            //Membuat pipa bergerak kesebelah kiri dengan kecepatan tertentu
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.gameObject.GetComponent<Bird>();
+
+        //Pengecekan Null value
+        if (bird)
+        {
+            //Mendapatkan komponent Collider pada game object
+            Collider2D collider = GetComponent<Collider2D>();
+
+            //Melakukan pengecekan Null varibale atau tidak
+            if (collider)
+            {
+                //Menonaktifkan collider
+                collider.enabled = false;
+            }
+
+            //Burung Mati
+            bird.Dead();
+        }
     }
 }
